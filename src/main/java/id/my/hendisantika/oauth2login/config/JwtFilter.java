d962ac63b24631b10fe01f7d165bd09287650a18
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:30:18 +0700

    feat: Add doFilterInternal in JwtFilter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/oauth2login/config/JwtFilter.java
+++ src/main/java/id/my/hendisantika/oauth2login/config/JwtFilter.java
@@ -1,10 +1,19 @@
 package id.my.hendisantika.oauth2login.config;
 
 import id.my.hendisantika.oauth2login.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +31,19 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            String username = jwtUtil.getUsernameFromToken(token);
+
+            if (username != null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+            }
+        }
+        chain.doFilter(request, response);
+    }
 }
