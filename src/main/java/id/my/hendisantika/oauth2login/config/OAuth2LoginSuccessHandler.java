40272ff5b8c4af16ec8b7b865ec144de2c63b6e6
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:31:15 +0700

    feat: Add onAuthenticationSuccess in OAuth2LoginSuccessHandler🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/oauth2login/config/OAuth2LoginSuccessHandler.java
+++ src/main/java/id/my/hendisantika/oauth2login/config/OAuth2LoginSuccessHandler.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.oauth2login.config;
 
 import id.my.hendisantika.oauth2login.service.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,14 @@ import org.springframework.stereotype.Component;
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final UserService userService;
 
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
+        if (userService.userExists(oAuth2User)) {
+            response.sendRedirect("/dashboard");
+        } else {
+            response.sendRedirect("/user-not-found");
+        }
+    }
 }
