3b3a28400ba97dd8b749fa47491737bbd7141c83
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:47:22 +0700

    feat: Add Testcontainers configs🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/oauth2login/SpringBootOauth2LoginApplicationTests.java
+++ src/test/java/id/my/hendisantika/oauth2login/SpringBootOauth2LoginApplicationTests.java
@@ -1,13 +1,38 @@
 package id.my.hendisantika.oauth2login;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.oauth2login.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SpringBootOauth2LoginApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17.4-alpine3.21")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @BeforeEach
+    void deleteAll() {
+        userRepository.deleteAll();
     }
 
 }
