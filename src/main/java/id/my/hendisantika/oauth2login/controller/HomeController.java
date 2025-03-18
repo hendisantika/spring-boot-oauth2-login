3a99a95cc96822c79b5c7094f5e6eeb9813b432a
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:32:53 +0700

    feat: Add HomeController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/oauth2login/controller/HomeController.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.oauth2login.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-oauth2-login
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/03/25
+ * Time: 05.32
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+public class HomeController {
+}
