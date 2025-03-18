7802160329e7237a29e09606c1b744900056f59c
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:27:53 +0700

    feat: Add User Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/oauth2login/entity/User.java
@@ -0,0 +1,45 @@
+package id.my.hendisantika.oauth2login.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-oauth2-login
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/03/25
+ * Time: 05.27
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String name;
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+}
