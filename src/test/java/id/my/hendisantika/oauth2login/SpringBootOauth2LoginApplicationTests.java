97f6c5ca36c0a493c249b49eeafa8d8d07cf88e4
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 04:39:27 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/oauth2login/SpringBootOauth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.oauth2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2LoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
