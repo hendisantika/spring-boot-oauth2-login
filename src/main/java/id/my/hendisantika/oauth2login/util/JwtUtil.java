41eedc72ae9f08c21e646518b16062f194bc5404
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:27:03 +0700

    feat: Add generateToken in JwtUtil🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/oauth2login/util/JwtUtil.java
+++ src/main/java/id/my/hendisantika/oauth2login/util/JwtUtil.java
@@ -1,6 +1,10 @@
 package id.my.hendisantika.oauth2login.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +21,12 @@ import org.springframework.stereotype.Component;
 public class JwtUtil {
     private final String SECRET_KEY = "one-piecejujutsu-kaisen"; // Use environment variables in production
 
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
 }
