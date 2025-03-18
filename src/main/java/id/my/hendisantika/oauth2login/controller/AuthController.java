f5d7a371c8479b29c41cf1dc685e4ae6d5ac397a
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:34:30 +0700

    feat: Add handleOAuth2Callback in AuthController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/oauth2login/controller/AuthController.java
+++ src/main/java/id/my/hendisantika/oauth2login/controller/AuthController.java
@@ -1,11 +1,17 @@
 package id.my.hendisantika.oauth2login.controller;
 
+import id.my.hendisantika.oauth2login.entity.User;
 import id.my.hendisantika.oauth2login.repository.UserRepository;
 import id.my.hendisantika.oauth2login.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +32,20 @@ public class AuthController {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
     private final OAuth2AuthorizedClientService authorizedClientService;
+
+    @GetMapping("/callback")
+    public String handleOAuth2Callback(OAuth2AuthenticationToken authentication) {
+        OAuth2User oAuth2User = authentication.getPrincipal();
+        String email = oAuth2User.getAttribute("email");
+
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent()) {
+            // Generate JWT Token
+            String token = jwtUtil.generateToken(email);
+            return "{\"status\": \"success\", \"token\": \"" + token + "\"}";
+        } else {
+            return "{\"status\": \"error\", \"message\": \"User not found in database\"}";
+        }
+    }
 
 }
