45a2c1ed419f0fa177c345bafbac0a7ee5c5c721
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:37:49 +0700

    feat: Add initData🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/oauth2login/SpringBootOauth2LoginApplication.java
+++ src/main/java/id/my/hendisantika/oauth2login/SpringBootOauth2LoginApplication.java
@@ -1,7 +1,13 @@
 package id.my.hendisantika.oauth2login;
 
+import id.my.hendisantika.oauth2login.entity.User;
+import id.my.hendisantika.oauth2login.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootOauth2LoginApplication {
@@ -10,4 +16,17 @@ public class SpringBootOauth2LoginApplication {
         SpringApplication.run(SpringBootOauth2LoginApplication.class, args);
     }
 
+
+    @Bean
+    public CommandLineRunner init(UserRepository userRepository) {
+        return (args) -> {
+            userRepository.deleteAll();
+            userRepository.saveAll(
+                    List.of(
+                            new User("admin@gmail.com", "admin"),
+                            new User("hendisantika@gmail.com", "hendisantika")
+                    )
+            );
+        };
+    }
 }
