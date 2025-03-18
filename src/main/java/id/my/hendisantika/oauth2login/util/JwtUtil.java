fa7d520132fc9d1af6701213a2918c14f95da1e7
hendisantika <hendisantika@yahoo.co.id>
2025-03-19 05:26:50 +0700

    feat: Add JwtUtil🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/oauth2login/util/JwtUtil.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.oauth2login.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-oauth2-login
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/03/25
+ * Time: 05.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "one-piecejujutsu-kaisen"; // Use environment variables in production
+
+}
